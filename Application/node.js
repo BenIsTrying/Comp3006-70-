a24e40e1a0d4b161dcdea6e55657e2ff2a774bdc
BenIsTrying <91667148+BenIsTrying@users.noreply.github.com>
2023-11-21 14:32:09 +0000

    basic editing

--- Application/node.js
+++ Application/node.js
@@ -1,11 +1,14 @@
 let http = require("http");
 let port = 9000;
+
 function sayHello() {
 return "Hello World from Node";
 }
+
 let server = http.createServer(function(request, response) {
 response.end(sayHello());
 });
+
 server.listen(port, function() {
 console.log("Server listening on port " + port);
 });
