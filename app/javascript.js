f4c6419c0c2429f141aa74bd2857fe627adb4731
BenIsTrying <benjamin.george-3@students.plymouth.ac.uk>
2024-01-06 22:18:35 +0000

    added booking page

---
+++ app/javascript.js
@@ -0,0 +1,13 @@
+$(function() {
+
+    
+
+    $("#booking").click(function booking(){
+            
+        window.location = 'booking.html';//allows user to access the booking page after picking their movie
+        
+    });
+
+
+
+});
